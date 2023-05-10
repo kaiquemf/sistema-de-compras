e73de6e01147954716880cc66c65823b278f6241
Kaique Ferreira <kaiquemartinsdasilvaferreira@gmail.com>
2023-05-10 18:26:43 -0300

    lógica da classe contemUsuario

--- sistemadecompras/src/br/com/sistemadecompras/ListaUsuarios.java
+++ sistemadecompras/src/br/com/sistemadecompras/ListaUsuarios.java
@@ -4,13 +4,21 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ListaUsuarios {
-	Set<String> listaDeUsuarios = new HashSet<String>();
+	static Set<String> listaDeUsuarios = new HashSet<String>();
+	
+	static void adicionaUsuario(String novoUsuario) {
+		listaDeUsuarios.add(novoUsuario);
+	}
 	
 	static void contemUsuario(String novoUsuario) {
-		
+		if (listaDeUsuarios.contains(novoUsuario)) {
+			System.out.println("Usuário já existente, por favor faça o login ou "
+					+ "crie um novo usuário");
+			OpcoesLogin.opcoesDeLogin();
+			CasesLogin.casesDeLogin();
+		} else {
+			adicionaUsuario(novoUsuario);
+		}
 	}
-	
-	static void adicionaUsuario() {
-		
-	}
+
 }
