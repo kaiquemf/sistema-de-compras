9b0e862d56e68f52daa5d3efa156e0e88748f461
Kaique Ferreira <kaiquemartinsdasilvaferreira@gmail.com>
2023-05-10 18:19:07 -0300

    criação classe de criação de usuários

---
+++ sistemadecompras/src/br/com/sistemadecompras/CriarUsuario.java
@@ -0,0 +1,11 @@
+package br.com.sistemadecompras;
+
+import java.util.Scanner;
+
+public class CriarUsuario {
+	static void criaUsuario() {
+		Scanner inputNovoUsuario = new Scanner (System.in);
+		
+		
+	}
+}
