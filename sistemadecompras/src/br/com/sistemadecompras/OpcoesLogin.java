2fc8c851a4203553a1db0c7c590b293e279fe5b3
Kaique Ferreira <101342267+kaiquemf@users.noreply.github.com>
2023-05-10 15:31:44 -0300

    criação classe de opções para login

---
+++ sistemadecompras/src/br/com/sistemadecompras/OpcoesLogin.java
@@ -0,0 +1,10 @@
+package br.com.sistemadecompras;
+
+public class OpcoesLogin {
+	static void opcoesDeLogin() {
+		System.out.println(
+		 "  1 - Entrar"
+		+"\n2 - Criar conta"
+	    +"\n3 - Sair");
+	}
+}
---
+++ sistemadecompras/src/br/com/sistemadecompras/SistemaPrincipal.java
@@ -0,0 +1,7 @@
+package br.com.sistemadecompras;
+
+public class SistemaPrincipal {
+	public static void main(String[] args) {
+		
+	}
+}
