bd90a63da6c7eaa255b8dfe06b5cc41c2951cf6e
Kaique Ferreira <kaiquemartinsdasilvaferreira@gmail.com>
2023-05-10 18:18:37 -0300

    criação classe listas de usuários

---
+++ sistemadecompras/src/br/com/sistemadecompras/ListaUsuarios.java
@@ -0,0 +1,16 @@
+package br.com.sistemadecompras;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ListaUsuarios {
+	Set<String> listaDeUsuarios = new HashSet<String>();
+	
+	static void contemUsuario(String novoUsuario) {
+		
+	}
+	
+	static void adicionaUsuario() {
+		
+	}
+}
