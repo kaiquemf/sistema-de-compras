8867660dd405f794c5fa0ef058a647c9472c6c43
Kaique Ferreira <kaiquemartinsdasilvaferreira@gmail.com>
2023-05-11 23:03:20 -0300

    sistema criação de senha

--- sistemadecompras/src/br/com/sistemadecompras/CriarSenha.java
+++ sistemadecompras/src/br/com/sistemadecompras/CriarSenha.java
@@ -1,7 +1,19 @@
 package br.com.sistemadecompras;
 
+import java.util.Scanner;
+
 public class CriarSenha {
+	static Scanner inputNovaSenha = new Scanner(System.in);
+
 	static void criaSenha() {
+		String novaSenha = inputNovaSenha.nextLine();
 		
+		if(novaSenha != null && !novaSenha.equals("") && novaSenha.length() >= 8) {
+			ListaSenhas.adicionaSenha(novaSenha);
+			System.out.println("Usuário criado com sucesso!");
+		} else {
+			System.out.println("Digite uma senha válida (mínimo de 8 dígitos): ");
+			criaSenha();
+		}
 	}
 }
--- sistemadecompras/src/br/com/sistemadecompras/ListaSenhas.java
+++ sistemadecompras/src/br/com/sistemadecompras/ListaSenhas.java
@@ -6,5 +6,7 @@ import java.util.List;
 public class ListaSenhas {
 	static List<String> listaDeSenhas = new ArrayList<String>();
 	
-	
+	static void adicionaSenha(String novaSenha) {
+		listaDeSenhas.add(novaSenha);
+	}
 }
